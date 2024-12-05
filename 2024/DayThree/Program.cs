5ea7837ef07500a59cf2f58ff8ca50e06f4f93bf
Zachary Zak <zachzak@hotmail.com>
2024-12-04 19:49:10 -0600

    Add daytwo part 1 and daythree part1

---
+++ 2024/DayThree/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FileInfo inputFile = new(args[0]);
+        //FileInfo inputFile = new(@"/home/zzak/Downloads/DayTwoPracticeInput.txt");
+        List<string> lines = [];
+        using (var reader = new StreamReader(inputFile.FullName))
+        {
+            while (!reader.EndOfStream)
+            {
+                lines.Add(reader.ReadLine());
+            }
+        }
+        var currentTotal = 0;
+            foreach (var line in lines)
+            {
+                currentTotal += ParseLine(line);
+            }
+
+            System.Console.WriteLine($"Current total = {currentTotal}");
+    }
+
+    private static int ParseLine(string line)
+    {
+        int lineTotal = 0;
+
+        Console.WriteLine(line);
+        Console.WriteLine("Matches:");
+        var reg = new Regex(@"mul\(\d+\,\d+\)");
+        var found = reg.Matches(line);
+        foreach (var match in found)
+        {
+            Console.WriteLine(match);
+            lineTotal += ParseAndCalculate(match);
+        }
+
+        return lineTotal;
+    }
+
+    private static int ParseAndCalculate(object mul)
+    {
+        var reg = new Regex(@"\d+");
+        var matches = reg.Matches(mul.ToString());
+        var number1 = matches.ElementAt(0).ToString();
+        var number2 = matches.ElementAt(1).ToString();
+
+        return Mul(int.Parse(number1), int.Parse(number2));
+    }
+
+
+    private static int Mul(int x, int y)
+    {
+        return x * y;
+    }
+}
---
+++ 2024/DayTwo/Program.cs
@@ -0,0 +1,125 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FileInfo inputFile = new(args[0]);
+        //FileInfo inputFile = new(@"/home/zzak/Downloads/DayTwoPracticeInput.txt");
+        List<string> lines = [];
+        using (var reader = new StreamReader(inputFile.FullName))
+        {
+            while (!reader.EndOfStream)
+            {
+                lines.Add(reader.ReadLine());
+            }
+        }
+
+        int safeCount = 0;
+        foreach (var line in lines)
+        {
+            var lineItems = ParseLine(line);
+            if (IsSafe(lineItems))
+            {
+                safeCount++;
+            }
+        }
+
+        System.Console.WriteLine($"the total number of safe lines = {safeCount}");
+
+        foreach (var line in lines)
+        {
+            var lineItems = ParseLine(line);
+            if (IsAnySafe(lineItems))
+            {
+                safeCount++;
+            }
+        }
+
+        System.Console.WriteLine($"the total number of safe lines after removing = {safeCount}");
+
+    }
+
+    private static List<int> ParseLine(string line)
+    {
+        List<int> numbers = [];
+
+        var lineItems = line.Split(' ');
+        foreach (var item in lineItems)
+        {
+            numbers.Add(int.Parse(item));
+        }
+
+        return numbers;
+    }
+
+    private static bool IsSafe(List<int> lineArray)
+    {
+        bool lastIncrease = false;
+        bool currentIncrease = false;
+        for (int i = 0; i < lineArray.Count(); i++)
+        {
+            if (i != lineArray.Count() - 1)
+            {
+                int num1 = lineArray[i];
+                int num2 = lineArray[i + 1];
+                int difference = num1 - num2;
+
+                if (num1 == num2)
+                {
+                    return false;
+                }
+
+                if (num1 < num2)
+                {
+                    currentIncrease = true;
+                }
+
+                if (num1 > num2)
+                {
+                    currentIncrease = false;
+                }
+
+                if(currentIncrease)
+                {
+                    difference = difference * -1;
+                }
+
+                if (difference > 3)
+                {
+                    return false;
+                }
+
+                if (i == 0)
+                {
+                    lastIncrease = currentIncrease;
+                }
+                else if (lastIncrease != currentIncrease)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+// this is for day two and is not working properly
+    private static bool IsAnySafe(List<int> lineArray)
+    {
+        var isSafe = IsSafe(lineArray);
+        if (!isSafe)
+        {
+            List<int> checkArray = [];
+            foreach (var num in lineArray)
+            {
+                checkArray.Clear();
+                checkArray.AddRange(lineArray);
+                checkArray.Remove(num);
+
+                isSafe = IsSafe(checkArray);
+
+            }
+        }
+
+        return isSafe;
+    }
+}
